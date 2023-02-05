ab297f39bff5cb831c8067a19f92aeabf87c28f6
Krima Patel <patel.krima@hotmail.com>
2023-02-05 16:27:04 -0600

    set up django project for cozy nest

---
+++ cozynest/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for cozynest project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cozynest.settings')
+
+application = get_asgi_application()
---
+++ cozynestapi/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CozynestapiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'cozynestapi'
